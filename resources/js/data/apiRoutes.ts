db199c2538fd4c9e126ac050ba0603c03ce4e57c
Dhaboav <100108392+Dhaboav@users.noreply.github.com>
2025-07-19 15:30:14 +0700

    docs: Update api documentation

--- resources/js/data/apiRoutes.ts
+++ resources/js/data/apiRoutes.ts
@@ -3,37 +3,39 @@ import { ApiRoute } from '@/types/api';
 export const apiRoutes: ApiRoute[] = [
     {
         method: 'POST',
-        uri: '/api/image',
-        description: 'Upload an image file.',
-        example: `POST /api/image
+        uri: '{URL}/api/upload-data',
+        description: 'Upload a data from sensor.',
+        example: `POST /api/upload-data
 Headers:
     Authorization: Bearer {api_token}
     Content-Type: multipart/form-data
 
 Body:
+    user_id: {id akun},
+    loc_id: {id lokasi},
+    is_trash: 1,
+    water_lvl: 0.90
     file: (image file)`,
 
         response: `{
-    "user"   : "Username",
-    "message": "Image uploaded successfully.",
-    "path"   : "/storage/uploads/gemastik.jpg"
+    "message": "Data added successfully",
 }`,
     },
     {
         method: 'DELETE',
-        uri: '/api/image',
-        description: 'Delete an uploaded image.',
-        example: `DELETE /api/image
+        uri: '{URL}/api/delete-data',
+        description: 'Delete a data information.',
+        example: `DELETE /api/delete-data
 Headers:
     Authorization: Bearer {api_token}
     Content-Type: application/json
 
 Body:
 {
-    "path": "/storage/uploads/gemastik.jpg"
+    "data_id": "{id}"
 }`,
         response: `{
-    "message": "Image deleted successfully."
+    "message": "Data deleted successfully"
 }`,
     },
 ];
